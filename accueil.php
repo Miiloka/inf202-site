09dbeeeb1e6d276699f353106d7791082a662c1c
“Miiloka” <magiczvirtuoz@gmail.com>
2024-02-18 19:48:23 +0100

    Modification mineures accueil

--- accueil.php
+++ accueil.php
@@ -50,32 +50,12 @@
     </nav>
   </header>
 
-<!-- Présentation rapide -->
-
-<main>
-  <div id="CarouselAvecSoustitre" class="carousel slide" data-bs-ride="carousel" >
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#CarouselAvecSoustitre" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#CarouselAvecSoustitre" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    </div>
-
-    <button class="carousel-control-prev" type="button" data-bs-target="#CarouselAvecSoustitre" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-
-    <button class="carousel-control-next" type="button" data-bs-target="#CarouselAvecSoustitre" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
-  </div>
-
 
 <!-- Description complète du site -->
-<div style="padding : 10px">
-  <h1 style="text-align : center">SERVICE D'OPHTALMOLOGIE</h1>
-<p>Le service d'ophtalmologie de l'hôpital est l'unité spécialisée dans le diagnostic, le traitement et la gestion des affections liées aux yeux et à la vision. Le service propose des consultations régulières pour les patients qui souhaitent des examens de routine pour évaluer leur vision, la santé de leurs yeux et détecter tout problème potentiel. Les ophtalmologues hospitaliers sont qualifiés pour diagnostiquer et traiter une variété de maladies oculaires telles que la cataracte, le glaucome, la dégénérescence maculaire, les infections oculaires, etc. Les services d'ophtalmologie hospitaliers sont souvent équipés pour réaliser diverses interventions chirurgicales oculaires, y compris la chirurgie de la cataracte, la correction de la vision au laser (LASIK), la chirurgie de rétine, etc. Dans le service nous assurons aussi les soins pédiatriques, les urgences oculaires, la rhéabilitation visuelle. Notre service est aussi impliqué dans la recherche et la formation des futurs ophtalmologues. Au total, notre service offre une gamme complète de soins oculaires, allant des examens de routine aux interventions chirurgicales complexes, avec une approche multidisciplinaire pour assurer la meilleure prise en charge possible des patients. </p>
-<p>Le service d'ophtalmologie est spécialisé dans la prise en charge des kératocône.  </p>
+<div>
+  <h1 style="text-align : center;">SERVICE D'OPHTALMOLOGIE</h1>
+<p>Le service d'ophtalmologie de l'hôpital est l'unité spécialisée dans le diagnostic, le traitement et la gestion des affections liées aux yeux et à la vision. Il propose des consultations régulières pour les patients souhaitant des examens de routine afin d'évaluer leur vision, la santé de leurs yeux et de détecter tout problème potentiel. Les ophtalmologues hospitaliers sont qualifiés pour diagnostiquer et traiter une variété de maladies oculaires telles que la cataracte, le glaucome, la dégénérescence maculaire, les infections oculaires, etc. Les services d'ophtalmologie hospitaliers sont souvent équipés pour réaliser diverses interventions chirurgicales oculaires, y compris la chirurgie de la cataracte, la correction de la vision au laser (LASIK), la chirurgie de la rétine, etc. Dans notre service, nous assurons également les soins pédiatriques, les urgences oculaires et la rééducation visuelle. De plus, notre service est impliqué dans la recherche et la formation des futurs ophtalmologues. En somme, notre service offre une gamme complète de soins oculaires, allant des examens de routine aux interventions chirurgicales complexes, avec une approche multidisciplinaire visant à assurer la meilleure prise en charge possible des patients.</p>
+<p>Le service d'ophtalmologie est spécialisé dans la prise en charge des kératocônes.</p>
 
 <div class="row" style="padding:10px;">
   <div class="column">
@@ -83,10 +63,10 @@
     <p>Le kératocône est une maladie de l'œil qui affecte la forme et l'épaisseur de la cornée, la partie transparente à l'avant de l'œil. Normalement, la cornée a une forme arrondie, agissant comme une lentille pour diriger la lumière vers la rétine. Cependant, dans le cas du kératocône, la cornée s'amincit progressivement et prend une forme conique irrégulière, ce qui provoque une déformation de la vision.</p>
     <h3>Caractéristiques</h3> 
 <ol>
-<li>Amincissement de la cornée;</li>
-<li>Déformation de la forme;</li>
-<li>Astigmatisme;</li> 
-<li>Troubles visuels;</li> 
+<li>Amincissement de la cornée</li>
+<li>Déformation de la forme</li>
+<li>Astigmatisme</li> 
+<li>Troubles visuels</li> 
 </ol>
 </div>
   <div class="column_img">
@@ -96,19 +76,10 @@
 
 
 <div class="row" style="padding:10px;">
-  <div class="column_img">
-    <img src="ressources/images/progres.png" alt="Progres" style="width :50%;">
-  </div>
-  <div class="column">
-  <h1 style="font-size : 50px">L'équipe</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem aut ratione nisi temporibus ad dignissimos quam vero qui unde suscipit quod at eius, voluptatem quaerat sed tempora praesentium consectetur molestias pariatur veritatis autem architecto et hic nostrum! Itaque perferendis harum, voluptatibus, ut, modi repudiandae ex nam a aspernatur molestias sit!</p>
-    <h2>Lorem 1</h2>
-    <p>Lorem ipsum dolor sit amet.</p>
-    <h2>Lorem 2</h2>
-    <p>Lorem ipsum dolor sit amet.</p>
-  </div>
-
+  <h1>ENGAGEMENTS</h1>
+    <p>Nous nous investissons également dans l'accessibilité des soins oculaires pour tous, en nous assurant que nos services sont disponibles et adaptés aux besoins individuels. Nous accordons une grande importance à l'éducation des patients pour favoriser une compréhension approfondie de leur santé visuelle. De plus, nous mettons un accent particulier sur la prévention des affections oculaires à travers des campagnes de sensibilisation et des examens réguliers.</p>
 </div>
+
 
 </main>
 
