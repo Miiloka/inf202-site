3ea70d28f4101a3352d27cd8b29086f98ca1c243
leapas15 <122348438+leapas15@users.noreply.github.com>
2024-02-21 22:58:19 +0100

    Amelioration de mes formulaires indiv

--- delete_patient_form.php
+++ delete_patient_form.php
@@ -20,24 +20,60 @@ include("connexion_base.php"); ?>
 </head>
 <body>
 
-<form method="POST" action="delete_patient.php" style=" width: 50vw; margin-left : 25vw;" >
+<br />
+  <style>
+       
+        body {
+            font-family: Arial;
+            background-color: #f4f4f4;
+            justify-content: center;
+            height: 10px;
+			font-size:13px;
+        }
+
+        form {
+            border: 2px solid #ccc;
+            padding: 10px;
+            border-radius: 8px;
+            background-color: white;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        input {
+            width: 100%;
+            padding: 8px;
+            margin-bottom: 10px;
+            box-sizing: border-box;
+        }
+
+        input[type="submit"] {
+            background-color: #77b5fe;
+            color: white;
+            cursor: pointer;
+        }
+    </style>
+
+<div style="max-width: 500px;"> 
+	<form method="POST" action="delete_patient.php" style=" width: 50vw; margin-left : 25vw;" >
 
 
-<fieldset><legend><h4> Supprimer un patient de la base informatique </h4></legend>
-<table>
-<tr> <td> Nom : </td><td><INPUT NAME="nom" TYPE="text"/> </td></tr>
-<tr><td> Prénom : </td><td><INPUT NAME="prenom" TYPE="text"/></td></tr>
-<tr><td> Numéro de dossier : </td><td><INPUT NAME="dossier" TYPE="text"/></td></tr>
-</table> 
+	<fieldset><legend><h4> Supprimer un patient de la base informatique </h4></legend>
+	<table>
+	<tr> <td> Nom : </td><td><INPUT NAME="nom" TYPE="text"/> </td></tr>
+	<tr><td> Prénom : </td><td><INPUT NAME="prenom" TYPE="text"/></td></tr>
+	<tr><td> Numéro de dossier : </td><td><INPUT NAME="dossier" TYPE="text"/></td></tr>
+	</table> 
 
-<input type="submit" name="bt1" value="Sortir le patient du service"/> <br />
+	<input type="submit" name="bt1" value="Sortir le patient du service"/> <br />
 
-</fieldset> <br /> 
-</form>
+	</fieldset> <br /> 
+	</form>
+	
+</div>
 </body>
 </html>
 
-
+<br />
 <?php include "model/footer.php"; ?>
 
 </body>
--- formulaire_co_equipe.php
+++ formulaire_co_equipe.php
@@ -2,34 +2,69 @@
 
 <?php include "model/debutpage.php"; ?>
 
+	
+		<meta charset="utf-8" />	
+		<title>Identification</title>
+</head>
 
 <body>
 
 <?php include "model/navbar.php"; ?>
-<html>
-	<head>
-		<meta charset="utf-8" />	
-		<title>Identification</title>
-</head>
-<body>
+<br />
+  <style>
+       
+        body {
+            font-family: Arial;
+            background-color: #f4f4f4;
+            justify-content: center;
+            height: 10px;
+			font-size:13px;
+        }
 
-<form method="POST" action="connexion_equipe.php" style=" width: 50vw; margin-left : 25vw;" >
+        form {
+            border: 2px solid #ccc;
+            padding: 10px;
+            border-radius: 8px;
+            background-color: white;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        input {
+            width: 100%;
+            padding: 8px;
+            margin-bottom: 10px;
+            box-sizing: border-box;
+        }
+
+        input[type="submit"] {
+            background-color: #77b5fe;
+            color: white;
+            cursor: pointer;
+        }
+    </style>
 
 
-<fieldset><legend><h4> Identification du personnel de service </h4></legend>
-<table>
-<tr> <td> Utilisateur : </td><td><INPUT NAME="utilisateur" TYPE="text"/> </td></tr>
-<tr><td> Mot de passe : </td><td><INPUT NAME="motpasse" TYPE="password"/></td></tr>
-</table> 
+<div style="max-width: 500px;"> 
+  
+	<form method="POST" action="connexion_equipe.php" style=" width: 50vw; margin-left : 25vw;" >
+	
+		<fieldset><legend><h4> Espace professionnels de santé </h4></legend>
+		<table>
+		<tr> <td> Utilisateur : </td><td><INPUT NAME="utilisateur" TYPE="text"/> </td></tr>
+		<tr><td> Mot de passe : </td><td><INPUT NAME="motpasse" TYPE="password"/></td></tr>
+		</table> 
 
-<input type="submit" name="bt1" value="Accéder à mon compte"/> <br />
+		<input type="submit" name="bt1" value="Accéder à mon compte"/> <br />
 
-</fieldset> <br /> 
-</form>
+		</fieldset> <br /> 
+		</form>
+</div>
+
+<br />
 </body>
-</html>
+
 
 <?php include "model/footer.php"; ?>
 
-</body>
+
 
