2d1f38076e22ead735f7c4d3d7f450b35597109b
leapas15 <122348438+leapas15@users.noreply.github.com>
2024-02-21 21:42:22 +0100

    MAJ FAQ

--- faq.php
+++ faq.php
@@ -16,46 +16,46 @@
     <h1 style = "text-align:center;">Questions fréquentes</h1>
 
     <div class="faq-cadre"> <!-- Question 1 -->
-      <input class="faq-cadre__trigger" id="faq-cadre-1" type="checkbox" /><label class="faq-cadre__titre" for="faq-cadre-1">Je souhaite enseigner, que dois-je faire ? </label>
+      <input class="faq-cadre__trigger" id="faq-cadre-1" type="checkbox" /><label class="faq-cadre__titre" for="faq-cadre-1">A quelle fréquence devrais-je passer un examen de la vue ?</label>
       <div class="faq-cadre__content-wrapper">
         <div class="faq-cadre__content">
           <p>
-            Tout d'abord, merci de vouloir contribuer à la réussite de ce site ! Il suffit de s'inscrire, puis votre inscription sera ou non validé par Adrien Orué, responsable pédagogique. Vous pouvez être amené à faire un entretien en visio pour répondre à de potentiels questions.
+            Il est recommandé de passer un examen de la vue tous les deux ans, même si vous ne pouvez présentez pas de symptômes. Cela permet de dépister les problèmes de vision au stade précoce.
           </p>
         </div>
       </div>
     </div>
 
     <div class="faq-cadre"> <!-- Question 2 -->
-      <input class="faq-cadre__trigger" id="faq-cadre-2" type="checkbox" /><label class="faq-cadre__titre" for="faq-cadre-2">Puis-je changer de formule à tout moment ?</label>
+      <input class="faq-cadre__trigger" id="faq-cadre-2" type="checkbox" /><label class="faq-cadre__titre" for="faq-cadre-2">Proposez-vous des consultations en ligne ?</label>
       <div class="faq-cadre__content-wrapper">
         <div class="faq-cadre__content">
           <p>
-            Bien-sûr ! Il faudra payer la différence si la formule est plus cher, ou vous serez remboursé la différence pour une formule à moindre prix.
+            Oui, certaines consultations peuvent être effectuées en ligne pour des problèmes oculaires mineurs. Cependant, pour des examens approfondis ou des problèmes graves, une consultation en personne est souvent nécessaire.
           </p>
         </div>
       </div>
     </div>
 
     <div class="faq-cadre"> <!-- Question 3 -->
-      <input class="faq-cadre__trigger" id="faq-cadre-3" type="checkbox" /><label class="faq-cadre__titre" for="faq-cadre-3">Comment puis-je vous contacter ?</label>
+      <input class="faq-cadre__trigger" id="faq-cadre-3" type="checkbox" /><label class="faq-cadre__titre" for="faq-cadre-3">Comment puis-je prendre rendez-vous ? </label>
       <div class="faq-cadre__content-wrapper">
         <div class="faq-cadre__content">
           <p>
-            Il suffit de cliquer sur la page "contact" pour une demande à tout le staff, ou "équipe" pour une demande personnel à un membre du staff, en bas du site, puis de remplir le formulaire.
+            Vous pouvez prendre rendez-vous en ligne sur Doctolib ou en nous appelant directement le secrétariat. Nous sommes là pour répondre à toutes vos questions et vous fournir les soins oculaires dont vous avez besoin.
           </p>
         </div>
       </div>
     </div>
 
     <div class="faq-cadre"> <!-- Question 4 -->
-      <input class="faq-cadre__trigger" id="faq-cadre-4" type="checkbox" /><label class="faq-cadre__titre" for="faq-cadre-4">Quels contenus sont disponibles ?
+      <input class="faq-cadre__trigger" id="faq-cadre-4" type="checkbox" /><label class="faq-cadre__titre" for="faq-cadre-4">Comment prendre soin de mes yeux au quotidien ? 
 
 </label>
       <div class="faq-cadre__content-wrapper">
         <div class="faq-cadre__content">
           <p>
-            Nous proposons des contenus pour toutes les matières pour les classes du CP à la Faculté. Tous ces contenus sont rédigés selon les programmes de l’Éducation Nationale, par des professionnels de l’édition scolaire. Vous pourrez retrouver des vidéos, des quiz, des exercices, des méthodes et bien plus !
+            Prenez des pauses régulières lorsque vous travaillez sur un écran, protégez vos yeux du soleil avec des lunettes de soleil appropriées, maintenez une alimentation saine riche en nutriments, et évitez le tabagisme pour préserver la santé de vos yeux.
           </p>
         </div>
       </div>
