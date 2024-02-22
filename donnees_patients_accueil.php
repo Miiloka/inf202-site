85aac6f8fca9ff3ef201f73f78ef72b7ece99265
leapas15 <122348438+leapas15@users.noreply.github.com>
2024-02-22 11:10:56 +0100

    formulaire perso

--- donnees_patients_accueil.php
+++ donnees_patients_accueil.php
@@ -10,9 +10,9 @@
 
 <?php include "model/navbar.php"; ?>
 
-<?php
-echo "Mettre les titres des formulaires quon va faire relatifs aux donnees des patients, et redirigeant vers le formulaire créer <br /> \n"; 
-echo "Pour sortir un patient du service : <a href='delete_patient_form.php'> Cliquer ici </a>"?>
+<form method ="POST" action="delete_patient_form.php">
+<input type="submit" name="ajout" value ="Supprimer un patient"/>
+</form>
 
 <form method ="POST" action="ajout_pat.php">
 <input type="submit" name="ajout" value ="Ajouter un patient"/>
