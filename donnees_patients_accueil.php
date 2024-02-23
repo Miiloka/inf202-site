32afb958a861a96b680092d97379f4b09be705d2
Maud Perpere <maud.perpere@etu.u-bordeaux.fr>
2024-02-23 09:24:38 +0100

    formulaires Maud

--- donnees_patients_accueil.php
+++ donnees_patients_accueil.php
@@ -18,6 +18,25 @@
 <input type="submit" name="ajout" value ="Ajouter un patient"/>
 </form>
 
+<form method = "POST" action="atcd_pers_pat.php">
+	<fieldset><h4> Chercher les antécédants personnels d'un patient </h4>
+	<table>
+	<tr> <td> Numéro de dossier : </td><td><INPUT NAME="num_dossier" TYPE="text"/> </td></tr>
+	</fieldset>
+<input type ="submit" name="cherche_atcd_pers" value = "Chercher les antécédants personnels d'un patient"/>
+</form>
+
+<form method = "POST" action="ajout_atcd_pers.php">
+	<fieldset><h4> Ajouter un antécédant personnel a un patient </h4>
+	<table>
+	<tr> <td> Numéro de dossier : </td><td><INPUT NAME="num_dossier" TYPE="text"/> </td></tr>
+	<tr> <td> Catégorie de l'antécédant : </td><td><INPUT NAME="Categorie_atcd" TYPE="text"/> </td></tr>
+	<tr> <td> Traitement actuel : </td><td><INPUT NAME="Traitement_act" TYPE="text"/> </td></tr>
+	<tr> <td> Traitement passé : </td><td><INPUT NAME="Traitement_passe" TYPE="text"/> </td></tr>
+	</fieldset>
+<input type ="submit" name="ajout_atcd_pers" value = "Ajouter un antécédant personnel a un patient"/>
+</form>
+
 <?php include "model/footer.php"; ?>
 
 </body>
