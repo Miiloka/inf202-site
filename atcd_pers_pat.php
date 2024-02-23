a9c887e0d5b1b9c29d95f4aed12fe996c495c404
Maud Perpere <maud.perpere@etu.u-bordeaux.fr>
2024-02-23 10:42:23 +0100

    actu Maud

--- atcd_pers_pat.php
+++ atcd_pers_pat.php
@@ -19,12 +19,12 @@
 include("connexion_base.php");
 
 // définition de la requête 
-$req = $pdo->prepare('select Categorie_atcd, count(Categorie_atcd) as nb_atcd from tab_atcdperso where Num_dossier = :num_user');
+$req = $pdo->prepare('select Categorie_atcd, count(Categorie_atcd) as nb_atcd from tab_atcdperso  where Num_dossier = :num_user group by Categorie_atcd');
 // exécution de la requête 
 $req->execute(array('num_user' => $_POST['num_dossier']));
 
 // Récupération des
-$ligne = $req->fetch();
+
 echo "Le patient ".$num_user, "a eu des antécédants de catégorie " .$Categorie_atcd ;
 ?>
 </body>
