bca922d4906050a4bd7d9ca9b94e4b8d41b852a8
Miiloka <magiczvirtuoz@gmail.com>
2024-02-23 10:16:30 +0100

    Navbar et corrections

---
+++ donnees_service.php
@@ -0,0 +1,16 @@
+<!-- Pour utiliser le CSS Bootstrap -->
+
+<?php include "model/debutpage.php"; ?>
+
+<title>Modele</title>
+
+</head>
+
+<body>
+
+<?php include "model/navbar.php"; ?>
+
+
+<?php include "model/footer.php"; ?>
+
+</body>
