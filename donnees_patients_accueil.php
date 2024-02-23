e6f428009bb53ae3e5005ea8ab46911b5e5e1af7
leapas15 <122348438+leapas15@users.noreply.github.com>
2024-02-23 15:34:50 +0100

    liste deroulante pour les fonctionnalite personnel

--- donnees_patients_accueil.php
+++ donnees_patients_accueil.php
@@ -1,26 +1,47 @@
-<?php include "model/debutpage.php"; ?>
 
-<title>Modele</title>
+<?php include "model/debutpage.php";
+// Vérifier si le formulaire a été soumis
+if (isset($_POST['redirect'])) {
+    // Rediriger l'utilisateur vers la page sélectionnée
+    $pageSelectionnee = $_POST['pageSelectionnee'];
+    header("Location: $pageSelectionnee");
+    exit; // Arrêter l'exécution du script après la redirection
+} 
+?>
 
+<style>
+        /* Augmenter la taille de la police pour le formulaire */
+        formusite {
+            font-size: 1.2em; /* Taille de la police augmentée */
+        }
+        /* Espacement autour du titre et du formulaire */
+        .container-formu {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            gap: 20px; /* Espacement entre les éléments */
+        }
+    </style>
 </head>
 
 <body>
-
 <?php include "model/navbar.php"; ?>
 
-<form method ="POST" action="delete_patient_form.php">
-<input type="submit" name="supp" value ="Supprimer un patient"/>
-</form>
-
-<form method ="POST" action="ajout_pat_form.php">
-<input type="submit" name="ajout" value ="Ajouter un patient"/>
-</form>
-
-<form method ="POST" action="chercher_ajout_atcd.php">
-<input type="submit" name="ajout" value ="Chercher ou ajouter un antécédent à un patient"/>
-</form>
-
+<div class="container-formu">
+    <h2>Choisissez une fonctionnalité</h2> <!-- Titre au-dessus du formulaire -->
+    <form class = "formusite" action="" method="post">
+        <select name="pageSelectionnee">
+            <option value="ajout_pat_form.php">Ajouter un patient au service</option>
+            <option value="delete_patient_form.php">Supprimer un patient du service</option>
+			<option value="chercher_ajout_atcd.php">Chercher ou ajouter un antécédent à un patient</option>
+			
+        </select>
+        <button type="submit" name="redirect">Accéder à la fonctionnalité</button>
+    </form>
+</div>
 
 <?php include "model/footer.php"; ?>
 
 </body>
+</html>
